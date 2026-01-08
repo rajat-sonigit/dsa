b9717f805a018ff74cf1b12a0a4c0c8830d18483
rajat-sonigit <rajatsonilvrs@gmail.com>
2026-01-08 22:05:15 +0530

    back triangle

--- main.java
+++ main.java
@@ -5,19 +5,13 @@ public class main {
  public static void main(String[] args) {
 Scanner sc =  new Scanner(System.in);
       int row = sc.nextInt();
-      for(int i = 1 ; i<=row ; i++){
-          for(int j =1; j<=row ; j++){
-            if (i %2 ==0){
-                  System.out.print(" "+(char)(i+96)+" ");
-            }else{
-                  System.out.print(" "+(char)(i+64)+" ");
+      for(int i = 1; i<=row ; i++){
+            for(int j = row; j>=i; j--){
+                 System.out.print(" * ");
             }
-           
-          }
-          System.out.println();
+            System.out.println();
+      
       }
-
-
 
 }   
 }
