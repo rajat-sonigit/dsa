1461a28cb50173130ab09678472c7eca40766b33
rajat-sonigit <rajatsonilvrs@gmail.com>
2026-01-09 23:44:43 +0530

    binary triangle

--- binarytriangle.java
+++ binarytriangle.java
@@ -7,12 +7,7 @@ public class binarytriangle {
         int row = sc.nextInt();
         for (int i = 1 ; i<= row ; i++){
             for (int j = 1; j<=i ;j++){
-                if ((i+j)%2 ==0){
-                    System.out.print(" 1 ");
-                }
-               else {
-                    System.out.print(" 0 ");
-                }
+               System.out.print(j*2 -1 + " ");
             }
             System.out.println();
         }
